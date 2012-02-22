b9eb40bb16bc9ad59c7ed938d204254df422ec86
Tim Novinger <tim.novinger@gmail.com>
2012-02-21 19:43:09 -0500

    Modified to allow for widget display post-signup success

--- mailchimp-widget.php
+++ mailchimp-widget.php
@@ -1,18 +1,18 @@
 <?php
 /*
 Plugin Name: MailChimp Widget
-Plugin URI: https://github.com/kalchas
-Description: 
-Author: James Lafferty
-Version: 0.7.1
-Author URI: https://github.com/kalchas
+Plugin URI: https://github.com/timnovinger/MailChimp-Widget
+Description: This plugin is the same as James Lafferty's 0.7.1 version, but modified to continue to display the widget after the user successfully signs up.
+Author: James Lafferty, Tim Novinger
+Version: 0.7.2
+Author URI: https://github.com/timnovinger/MailChimp-Widget
 License: GPL2
 */
 
 /*  Copyright 2010  James Lafferty  (email : james@nearlysensical.com)
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -34,24 +34,24 @@ set_include_path(get_include_path() . PATH_SEPARATOR . realpath(dirname(__FILE__
 spl_autoload_extensions('.class.php');
 
 if (! function_exists('buffered_autoloader')) {
-	
+
 	function buffered_autoloader ($c) {
 
 		try {
-		
+
 			spl_autoload($c);
-			
+
 		} catch (Exception $e) {
-			
+
 			$message = $e->getMessage();
-			
+
 			return $message;
-			
+
 		}
-		
+
 
 	}
-	
+
 }
 
 spl_autoload_register('buffered_autoloader');
